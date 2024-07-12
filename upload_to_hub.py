27a4cee34331b95368568e34fa04721d60e321cc
barney49 <ben.amwithyou@gmail.com>
2024-07-12 13:56:36 +0000

    feat: Add script for uploading dataset from s3 to huggingface hub

---
+++ upload_to_hub.py
@@ -0,0 +1,24 @@
+import dask.dataframe as dd
+from datasets import Dataset, DatasetDict
+import s3fs
+
+BUCKET_NAME = 'hf-datatrove'
+DUMP_TO_PROCESS = 'CC-MAIN-2024-10'
+
+# This is path for final result
+s3_path = f's3://{BUCKET_NAME}/minhash/{DUMP_TO_PROCESS}/deduped_output/**/*.jsonl.gz'
+
+fs = s3fs.S3FileSystem()
+
+files = fs.glob(s3_path)
+
+df = dd.read_json(s3_path, blocksize=None, compression='gzip')
+
+pandas_df = df.compute()
+s3_files = [f's3://{file}' for file in files]
+
+hf_dataset = Dataset.from_pandas(pandas_df)
+
+dataset_dict = DatasetDict({"train": hf_dataset})
+
+dataset_dict.push_to_hub("barney49/test")
