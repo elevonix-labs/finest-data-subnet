6d9b212bc5e3744aff98bba48308d98e5f004cae
barneylogo <ben.amwithyou@gmail.com>
2025-01-14 01:47:07 +1200

    feat: Add check similarity script

--- validator/process_commit/main.py
+++ validator/process_commit/main.py
@@ -4,6 +4,7 @@ import json
 import requests
 import os
 from utils import extract_commit
+from check_similarity import DataProcessor
 def process_commits(redis_queue: redis.Redis):
     """
     Async task to process commits from the queue.
@@ -21,13 +22,18 @@ def process_commits(redis_queue: redis.Redis):
                 commit_block = commit_data['commit_block']
                 hf_url = extract_commit(current_commit)
                 print(hf_url)
-                # response = requests.post(f"{os.getenv('API_URL')}/check-dataset/", json={"uid": int(uid)})
-                # print(response)
+                response = requests.post(f"{os.getenv('API_URL')}/subnets/check-task/", json={"uid": int(uid)})
+                data = response.json()
+                warc_files = data.get('warc_files')
+                request_block = data.get('request_block')
+                data_processor = DataProcessor(warc_files, hf_url)
+                sample_similarities = data_processor.run()
+
             else:
                 print("No commit found")
         except Exception as e:
             print(f"Error: {e}")
-        time.sleep(10)
+        time.sleep(100)
 
 if __name__ == "__main__":
 
